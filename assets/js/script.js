3dce7d0023042b2160528f9f106ebf281c2a5694
Alexander Disput <alexanderdisput@yahoo.com>
2023-12-11 07:54:22 +0000

    changed the behaviour of the way the rules are shown and added the overlay for the image choice for the player. Instead of changing the innerHTML of the blur-cover id, I am completely replacing it.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,66 @@
-// run game funtion that will execute the game once the button start game or start new game has been clicked 
+// run game funtion that will execute the game once the button start game or start new game has been clicked
 
-// function to give all buttons a job:
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
+/**
+ * The rulebook will provide the comparison table to evaluate a winner in the round
+ */
+var rulebook = {
+  Satellite: ["Spaceship", "Alien"],
+  Meteorite: ["Satallite", "Laser"],
+  Spaceship: ["Meteorite", "Laser"],
+  Laser: ["Alien", "Satellite"],
+  Alien: ["Meteorite", "Spaceship"],
+};
 
-    for (let button of buttons) {
-        button.addEventListener("click", function (){
-            if (this.getAttribute("data-type") == "submit-name") {
-                storeName();
-                changeName(userName);
-                showRules();
-            }
-        })
+/**
+ * This function gives every button in the DOM the eventlistener and makes sure that buttons that are generated
+ * dynamically also have the same event listener attached to them
+ */
+document.addEventListener("DOMContentLoaded", function () {
+  // let buttons = document.getElementsByTagName("button");
+
+  document.addEventListener("click", function (event) {
+    if (
+      event.target.tagName === "BUTTON" &&
+      event.target.getAttribute("data-type") === "submit-name"
+    ) {
+      storeName();
+      showRules();
+    } else if (
+      event.target.tagName === "BUTTON" &&
+      event.target.getAttribute("data-type") === "start-game"
+    ) {
+      event.preventDefault();
+      runGame();
+    } else if (
+      event.target.tagName === "BUTTON" &&
+      event.target.getAttribute("data-type") === "show-rules"
+    ) {
+      event.preventDefault();
+      showRules();
     }
-})
+  });
+});
 
-// Funtion that will collect the name once the screen is loaded and saved to a name = variable 
+/**
+ * This function collects the name from the userinput in the welcome screen and replaces YOU with the players name
+ */
 var userName;
-function storeName(){ 
-    userName = document.getElementById("player-name").value
+function storeName() {
+  userName = document.getElementById("player-name").value;
+  let playerName = document.getElementById("player-name-headline");
+  playerName.textContent = userName.toUpperCase();
 }
 
-
-// function that changes the name shown on the screen from that name = variable
-function changeName(userName) {
-    let playerName = document.getElementById("player-name-headline")
-    playerName.textContent = userName.toUpperCase()
-}
-
-// function to use the storeName and change Name function once the Next submit button 
+// function to use the storeName and change Name function once the Next submit button
 // is clicked which will also bring show the rules
 function showRules() {
+    let toRemove = document.getElementById("blur-cover");
+    if (toRemove){
+        toRemove.remove();
+    }
+    
     let html = `
+    <div id="blur-cover">
     <div id="game-rules">
         <h1>The Rules</h1>
         <h2>This game is a variation of the popular game Rock Paper Scissors with the following setup:</h2>
@@ -42,47 +71,67 @@ function showRules() {
         <p><span>Alien</span> defeats Meteorite and Spaceship.</p>
         <button data-type="start-game">Let's Go!</button>
     </div>
-    `
-    let div = document.getElementById("blur-cover")
-    div.innerHTML = html
+    </div>
+    `;
+    let container = document.createElement("div");
+    container.innerHTML = html;
+    console.log(container)
+    document.body.appendChild(container.firstElementChild)
 }
 
-
-
-// function that will increment player score 
-// function that will increment computer score 
+// function that will increment player score
+// function that will increment computer score
 
 // funtion to reset scores
 
-// function to show rules 
-
-// function to choose an image from the image array for computer and add it to the DOM 
+// function to choose an image from the image array for computer and add it to the DOM
 // function to compare images and determine winner through dictionary
 
-// funtion to create div with image choices and save the choice of player
 
+function runGame() {
+    // removes the overlay if it is there
+    let toRemove = document.getElementById("blur-cover");
+    if (toRemove){
+        toRemove.remove();
+    }
+  let html = `
+  <div id="blur-cover">
+    <div id="player-pick-outer">
+        <h2>Make your choice</h2>
+        <div class="player-pick-inner">
+        <a href="#"><img data-type="alien" src="assets/images/alien.webp" alt="Image of an alien"></a>
+        <a href="#"><img data-type="spaceship" src="assets/images/spaceship.webp" alt="Image of a spaceship"></a>
+        <a href="#"><img data-type="meteor" src="assets/images/meteor.webp" alt="Image of a meteor"></a>
+        <a href="#"><img data-type="satellite" src="assets/images/satellite.webp" alt="Image of a satellite"></a>
+        <a href="#"><img data-type="laser" src="assets/images/laser.webp" alt="Image of a laser"></a>
+        </div>
+    </div>
+    </div>
+    `;
+    let container = document.createElement("div");
+    container.innerHTML = html;
+    console.log(container)
+    document.body.appendChild(container.firstElementChild)
 
-// function welcomeScreen() {
-//     let html = `
+//   Computer chooses the image
+    
 
-//     `
-// }
+document.addEventListener("click", function(event){
+    if(event.target.tagName === "IMG" && event.target.getAttribute("data-type") === "alien") {
+        event.preventDefault();
 
+    }
+})
+}
 
+// pseudo code:
 
+// function create choices:
+//     create html element with images
+//     add event listeners to the images
+//     save the choice to the var player choice
 
-
-
-
-
-
-
-
-
-
-
-
-// How to insert HTMl into DOM: 
+// // How to insert HTMl into DOM:
 // let html = `
 //   <table>
 //     <thead>
